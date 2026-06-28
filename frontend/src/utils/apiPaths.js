cd0988701e8394fd9551e1568c4aec9a47b85804
Harsh Patel <harshpatel15505@gmail.com>
2026-06-28 21:44:55 +0530

    added production urls

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-//export const  API_BASE_URL = import.meta.env.VITE_API_URL ;
-export const  API_BASE_URL =  "http://localhost:8000";
+export const  API_BASE_URL = import.meta.env.VITE_API_URL ;
+//export const  API_BASE_URL =  "http://localhost:8000";
 export const API_PATHS = {
     AUTH:{
         LOGIN:"/api/v1/users/login",
